55ee1a7fca84b498a6e58dfd33f8c16090f50589
sunggyupaik <melon9751@naver.com>
2024-06-03 17:22:13 +0900

    init

--- vue-cli/src/views/AskView.vue
+++ vue-cli/src/views/AskView.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk" :key="item.id">
-      <!-- <a v-bind:href="item.url">
-        {{ item.title }}
-      </a> -->
-      <router-link v-bind:to="`/item/${item.id}`">{{ item.title }}</router-link>
-      <small>
-        {{ item.time_ago }} by {{ item.user }}
-      </small>
-    </p>
+    <ul class="ask-list">
+      <li v-for="item in fetchedAsk" :key="item.id" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="ask-title">
+            <router-link v-bind:to="`/item/${item.id}`">
+            {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by 
+            <router-link v-bind:to="`/item/${item.user}`" class="link-text">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,4 +34,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+  .ask-list {
+    margin: 0;
+    padding: 0;
+  }
+  .post {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+  }
+  .points {
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .ask-title {
+    margin: 0;
+  }
+  .link-text {
+    color: #828282;
+  }
+</style>
--- vue-cli/src/views/JobsView.vue
+++ vue-cli/src/views/JobsView.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <p v-for="job in this.$store.state.jobs" :key="job.id">
-      <a v-bind:href="job.url">
-        {{ job.title }}
-      </a>
-      <small>
-        {{ job.time_ago }} by {{ job.domain }}
-      </small>
-    </p>
+    <ul class="jobs-list">
+      <li v-for="job in this.$store.state.jobs" :key="job.id" class="post">
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <div>
+          <p class="jobs-title">
+            <a v-bind:href="job.url">
+            {{ job.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }} by 
+            <a v-bind:href="job.url">
+              {{ job.domain }}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,5 +32,28 @@ export default {
 </script>
 
 <style>
-
+  .jobs-list {
+    margin: 0;
+    padding: 0;
+  }
+  .post {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+  }
+  .points {
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .jobs-title {
+    margin: 0;
+  }
+  .link-text {
+    color: #828282;
+  }
 </style>
